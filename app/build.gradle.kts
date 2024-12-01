add35bc34bac60b505f4a95ca1bf9198fc8cf6b4
Bazo4622 <bazerlyanthony2@gmail.com>
2024-12-01 17:40:48 +0200

    Issue noticed when running app, it crashes immediately. After meticulous debugging, JDK 11 was installed and JDK 17 was needed, and, issue with dependencies found, were classes were being duplicated, so after excluding each duplication, and installation of required new dependencies, the app finally stopped crashing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "com.usj.calendarapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.usj.calendarapp"
@@ -36,18 +37,24 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.room.common)
-    implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.room.runtime.android)
-    implementation(libs.androidx.room.runtime.jvm)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.room.common){
+        exclude(group = "org.jetbrains", module = "annotations")
+    }
+    implementation(libs.androidx.room.ktx){
+        exclude(group = "org.jetbrains", module = "annotations")
+    }
+    implementation(libs.androidx.room.runtime.android) {
+        exclude(group = "org.jetbrains", module = "annotations")
+    }
+}
 
+configurations.all {
+    resolutionStrategy {
+        force("org.jetbrains:annotations:23.0.0")
+    }
 }
